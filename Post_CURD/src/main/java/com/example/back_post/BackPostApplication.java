7ceb2de5ef17f8eae47374597b4c51ba34aacc0a
velyn666 <ni1722650@gmail.com>
2023-07-11 17:44:07 +0900

    Create : Stetting

---
+++ Post_CURD/src/main/java/com/example/back_post/BackPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPostApplication.class, args);
+    }
+
+}
---
+++ Post_CURD/src/test/java/com/example/back_post/BackPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
